3b9585fb5cfea940734ab449b475e4b08dda0231
Dinesh Kumar D <dinesh.kumar4@allen.in>
2025-09-10 12:05:57 +0530

    chore: cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import Grid from "@/components/calendar/Grid";
-import Header from "@/components/calendar/Header";
-import Timeline from "@/components/calendar/TimeLine";
-// (new Date().getDay()) // Current day of the week (0-6)
-
-
 export default function Home() {
 
   return (
     <div className="flex flex-row">
-      <Timeline />
       <div className="flex flex-col w-full">
-        <Header />
-        <Grid />
+        <h1>Home</h1>
       </div>
     </div>
   );
